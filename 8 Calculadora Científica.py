a2aedb7b8878f6d5a17d230ec7c9f9b32c6bbf36
Gabriel Arienti Barbieri <gabriel17out@hotmail.com>
2022-08-20 09:28:54 -0300

    Create 8 Calculadora Científica.py

---
+++ 8 Calculadora Científica.py
@@ -0,0 +1,26 @@
+from tkinter import *
+from tkinter import ttk
+
+import math
+################# cores ###############
+co1 = "#feffff"  # white/branca
+co2 = "#6f9fbd"  # blue/azul
+co3 = "#38576b"  # valor
+
+fundo = "#e8e6e6"
+co10 ="#363434"
+cor11 ="#424345"
+
+cor1='#FFAB40'
+cor2='#ff333a'
+cor3='#6bd66f'
+cor4="#ab8918"
+
+janela = Tk()
+janela.title('')
+janela.geometry('235x289')
+janela.configure(bg=co1)
+
+
+style = ttk.Style(janela)
+style.theme_use("clam")
