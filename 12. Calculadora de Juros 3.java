eb3d9ae39ba1ffedd05d05f44f410864893e14d4
Gabriel Arienti Barbieri <gabriel.arienti@gmail.com>
2022-09-15 17:22:46 -0300

    Update 12. Calculadora de Juros 3.java

--- 12. Calculadora de Juros 3.java
+++ 12. Calculadora de Juros 3.java
@@ -4,9 +4,8 @@ import java.util.Scanner;
 public class Main {
     final static byte MESES_DO_ANO = 12;
     final static byte PORCENTAGEM = 100;
-    
+
     public static void main(String[] args) {
-        int numeroDePagamentos;
         double totalPago ;
 
         System.out.println("\nCalculadora de Empréstimo");
@@ -20,23 +19,36 @@ public class Main {
         String valorMensalFormatado = NumberFormat.getCurrencyInstance().format(valorMensal);
         System.out.println("Parcelas mensais no valor de: " + valorMensalFormatado);
 
+        totalPago = totalPago(anos, valorMensal);
+        totalDeJuros(totalPago, valorTotal);
+        exibirParcelas(valorTotal, jurosAnuais, anos);
+    }
 
-        totalPago = (valorMensal * (anos * MESES_DO_ANO));
-        String totalFormatado = NumberFormat.getCurrencyInstance().format(totalPago);
-        System.out.println("\nO valor total a ser pago é de:" + totalFormatado);
-
-        int valorDosJuros = (int)(totalPago - valorTotal);
-        String valorJurosFormatado = NumberFormat.getCurrencyInstance().format(valorDosJuros);
-        System.out.println("Total de juros: " + valorJurosFormatado);
+    private static void exibirParcelas(int valorTotal, float jurosAnuais, byte anos) {
         System.out.println();
         System.out.println("--------------");
         System.out.println("Agenda de Pagamentos Mensais:");
         System.out.println("--------------");
-        for (short meses = 1; meses <= anos * MESES_DO_ANO; meses++){
+        for (short meses = 1; meses <= anos * MESES_DO_ANO; meses++) {
             double balanco = calcularPagamentos(valorTotal, jurosAnuais, anos, meses);
             System.out.println(meses + "- " + NumberFormat.getCurrencyInstance().format(balanco));
         }
     }
+
+    private static double totalPago(byte anos, double valorMensal) {
+        double totalPago;
+        totalPago = (valorMensal * (anos * MESES_DO_ANO));
+        String totalFormatado = NumberFormat.getCurrencyInstance().format(totalPago);
+        System.out.println("\nO valor total a ser pago é de:" + totalFormatado);
+        return totalPago;
+    }
+
+    private static void totalDeJuros(double totalPago, int valorTotal) {
+        int valorDosJuros = (int)(totalPago - valorTotal);
+        String valorJurosFormatado = NumberFormat.getCurrencyInstance().format(valorDosJuros);
+        System.out.println("Total de juros: " + valorJurosFormatado);
+    }
+
     public static double lerValor(String promt, double min, double max){
         Scanner scanner = new Scanner (System.in);
         double valor;
@@ -51,7 +63,7 @@ public class Main {
     }
 
     public static double calcularPagamentos(int valorTotal, float jurosAnuais, byte anos, short numeroDeParcelasPagas){
-        
+
         float jurosMensais = (jurosAnuais / PORCENTAGEM) / MESES_DO_ANO;
         short numeroDePagamentos = (short)(anos * MESES_DO_ANO);
 
